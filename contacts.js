ceca57d9842aca091ffa7965c137dc6129f50e28
Yuliia Karas <iuliiakaras@gmail.com>
2023-07-29 06:57:23 +0200

    step1

---
+++ contacts.js
@@ -0,0 +1,31 @@
+// "Hello,contacts!"
+// const admins = ["Jhon", "Jake", "Jhosh"];
+// const clients = ["Olya", "Nina", "Masha"];
+
+// const users = { admins, clients };
+
+// module.exports = users;
+
+// contacts.js
+
+/*
+ * Розкоментуй і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту. 
+}
